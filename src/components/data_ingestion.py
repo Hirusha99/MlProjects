0a2f5a38b496e3736e00ffd4fc8c42fd9fe5672d
Hirusha99 <hirushanethni2277@gmail.com>
2024-06-22 22:59:16 +0530

    data transformation step done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,8 +7,12 @@ import pandas as pd
 from dataclasses import dataclass
 from sklearn.model_selection import train_test_split
 import os
+from src.components import data_transformation
 from src.logger import logging
 from src.exception import CustomException
+
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:  # these are the inputs that i provide data ingestion components, now data ingestion know where to save data
@@ -49,9 +53,14 @@ class DataIngestion:
             )
         except Exception as e:
             raise CustomException(e, sys)
-
+        
 
 if __name__ == '__main__':
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
+
+
 
